595fd090846cfd6c1a8e9eba40b4b47d36ce9b13
Sardius <0xSardius@gmail.com>
2025-08-18 12:45:33 -0400

    route handling complete

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,7 +2,22 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme")?.value;
+
+    if (!themePreference) {
+        response.cookies.set("theme", "dark");
+    }
+
+
+    // return NextResponse.redirect(new URL("/", request.url));
+
+    // if (request.nextUrl.pathname === "/profile") {
+    //     return NextResponse.redirect(new URL("/", request.url));
+    // }
+
+    return response;
 }
 
 export const config = {
